62421d4140905c092f72a0dd86f1df048098c431
likaixin <20231112@stu.neu.edu.cn>
2025-03-25 18:37:26 +0800

    新增一些多样数据

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -5,6 +5,6 @@ export const STUDENTS: Student[] = [
     {studentId: '20231114', studentName: '哈基b', studentBirthday:new Date(2004,7,12), isMale: true},
     {studentId: '20231115', studentName: 'gcy', studentBirthday:new Date(2005,3,3), isMale: true},
     {studentId: '20231116', studentName: 'zzy', studentBirthday:new Date(2004,11,2), isMale: false},
-    {studentId: '20231117', studentName: 'lxy', studentBirthday:new Date(2005,1,3), isMale: false},
-    {studentId: '20231118', studentName: 'wxy', studentBirthday:new Date(2004,10,12), isMale: false},
+    {studentId: '20231117', studentName: 'lxy', studentBirthday:new Date(2025,1,3), isMale: false},
+    {studentId: '20231118', studentName: 'wxy', studentBirthday:new Date(1972,10,12), isMale: false},
 ]
